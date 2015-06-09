da973978b72ba8be55a4dc88c21515fb4c182446
Sarah Foregger <sarahfo@Sarahs-MacBook-Pro-2.local>
2015-06-08 18:11:53 -0700

    added commas to popup prices

--- static/js/script.js
+++ static/js/script.js
@@ -17,5 +17,19 @@ $(document).ready(function(){
   });	
     	 
 
+// function addCommas(nStr)
+// {
+//       nStr += '';
+//       x = nStr.split('.');
+//       x1 = x[0];
+//       x2 = x.length > 1 ? '.' + x[1] : '';
+//       var rgx = /(\d+)(\d{3})/;
+//       while (rgx.test(x1)) {
+//         x1 = x1.replace(rgx, '$1' + ',' + '$2');
+//       }
+//       return x1 + x2;
+// }
 
-
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
